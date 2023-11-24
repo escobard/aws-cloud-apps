5767e1b0fa1773eddc033b47fccf4af9f1af0821
escobard <escobard90@gmail.com>
2023-11-24 15:15:07 -0700

    feat: add modular resolvers & types

---
+++ server/graphql/typeDefs.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import url from 'url';
+import util from 'util';
+
+import { loadFiles } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const typesArray = await loadFiles(path.join(__dirname, 'types'), {
+        ignoreIndex: true,
+        requireMethod: async (path) => {
+            return await import(url.pathToFileURL(path));
+        },
+    });
+
+const types = mergeTypeDefs(typesArray);
+
+export default types;
