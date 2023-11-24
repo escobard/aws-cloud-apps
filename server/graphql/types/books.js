cda7541148fd3cb31a5f4bc6a29ad21d75bad987
escobard <escobard90@gmail.com>
2023-11-24 16:00:18 -0700

    feat: add note types

--- server/graphql/types/books.js
+++ server/graphql/types/books.js
@@ -1,4 +1,4 @@
-const typeDefs = `#graphql
+const typeDefs = `
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
