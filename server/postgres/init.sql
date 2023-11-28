652539c2b10f298a85fc4b4148485c97a073d469
escobard <escobard90@gmail.com>
2023-11-28 16:38:51 -0700

    doc: update init.sql schema with TODO's for future improvement

--- server/postgres/init.sql
+++ server/postgres/init.sql
@@ -1,17 +1,19 @@
 CREATE SCHEMA notes;
 
+-- TODO - remove users table & foreign keys until auth0 w/ google account signin is implemented
 CREATE TABLE notes.users (
-    id serial,
+    id SERIAL PRIMARY KEY,
     email VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE notes.notes (
-    id serial,
-    user_id INTEGER NOT NULL,
+    id SERIAL PRIMARY KEY,
+    user_id REFERENCES notes.users(id) ON DELETE CASCADE INTEGER NOT NULL,
     subject VARCHAR(250) NOT NULL,
     note VARCHAR(1000) NOT NULL,
-    date VARCHAR(25) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES notes.users(id) ON DELETE CASCADE,
-    PRIMARY KEY (id)
+    -- TODO - change to pg generated timestamp, add created/updated/removed at columns
+    date VARCHAR(25) NOT NULL
 );
+
+-- TODO insert a sample note for demo purposes
