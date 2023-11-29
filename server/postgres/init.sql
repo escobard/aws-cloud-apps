545c17a1395aaaf30627aa669751434ff6546114
escobard <escobard90@gmail.com>
2023-11-28 17:24:05 -0700

    fix: postgresql schema typos

--- server/postgres/init.sql
+++ server/postgres/init.sql
@@ -3,13 +3,12 @@ CREATE SCHEMA notes;
 -- TODO - remove users table & foreign keys until auth0 w/ google account signin is implemented
 CREATE TABLE notes.users (
     id SERIAL PRIMARY KEY,
-    email VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
+    email VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE notes.notes (
     id SERIAL PRIMARY KEY,
-    user_id REFERENCES notes.users(id) ON DELETE CASCADE INTEGER NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES notes.users(id),
     subject VARCHAR(250) NOT NULL,
     note VARCHAR(1000) NOT NULL,
     -- TODO - change to pg generated timestamp, add created/updated/removed at columns
