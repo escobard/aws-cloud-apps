693331081ba985199c766c71cc631186e9fd0d4a
escobard <escobard90@gmail.com>
2023-12-04 07:32:51 -0700

    feat: add create note resolver
    also fix getNotes typos

--- server/graphql/resolvers/helpers/getNotes.js
+++ server/graphql/resolvers/helpers/getNotes.js
@@ -3,11 +3,13 @@ import cache from "../../cache.js";
 const getNotes = async () => {
     try{
         const notes  = cache.keys()
+        const fetchedNotes = []
         notes.map(id => {
             let note = cache.get(id)
-            cache.set(note.id, note)
+            fetchedNotes.push(note)
+            // cache.set(note.id, note)
         });
-        return notes;
+        return fetchedNotes;
     }
     catch(err){
         return err;
--- server/graphql/resolvers/notes.js
+++ server/graphql/resolvers/notes.js
@@ -1,9 +1,16 @@
 import getNotes from "./helpers/getNotes.js"
+import createNote from "./helpers/createNote.js";
 
 const resolver = {
     Query: {
         getNotes(parent){
             return getNotes(parent);
+        }
+    },
+    Mutation: {
+        createNote(parent, { note }){
+            console.log(note)
+            return createNote(parent, note)
         }
     }
 };
