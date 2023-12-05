2d890b54a9ddd66255d74c1325b58d45e9fda364
escobard <escobard90@gmail.com>
2023-12-05 12:04:53 -0700

    feat: add knex to getNotes and apply caching

--- server/graphql/resolvers/helpers/getNotes.js
+++ server/graphql/resolvers/helpers/getNotes.js
@@ -1,15 +1,29 @@
+import { knexConnector } from "../../utils/knexConnector.js"
+
 import cache from "../../cache.js";
 
 const getNotes = async () => {
     try{
-        const notes  = cache.keys()
-        const fetchedNotes = []
-        notes.map(id => {
-            let note = cache.get(id)
-            fetchedNotes.push(note)
-            // cache.set(note.id, note)
-        });
-        return fetchedNotes;
+        let knex = knexConnector();
+        let notes;
+        // checks to see if cache has data before calling DB
+        /// if cache has no data, fetch data from database
+        if (cache.keys().length === 0){
+            notes = await knex('notes.notes').select('*');
+            // populate cache with DB data after initial fetch
+            notes.map(note => {
+                cache.set(note.id, note)
+            })
+        }
+        /// if cache has data, fetch data from cache instead of db
+        else {
+            notes = []
+            cache.keys().map(id => {
+                let note = cache.get(id)
+                notes.push(note)
+            });
+        }
+        return notes;
     }
     catch(err){
         return err;
