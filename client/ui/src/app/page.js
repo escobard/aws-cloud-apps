601c81a5398ea772a8fdd88cba86a5358705e1a9
escobard <escobard90@gmail.com>
2023-12-05 15:59:16 -0700

    feat: add Home component to landing page

--- client/ui/src/app/page.js
+++ client/ui/src/app/page.js
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+import Landing from "../pages/Home";
+
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Landing />
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
