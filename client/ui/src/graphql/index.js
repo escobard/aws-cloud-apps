3e204b33159bb6bbfa269ec88ad71bbf9336c800
escobard <escobard90@gmail.com>
2023-12-06 11:29:34 -0700

    fix: missing headers for Apollo Server to play nicely

--- client/ui/src/graphql/index.js
+++ client/ui/src/graphql/index.js
@@ -1,10 +1,27 @@
 import { onError } from '@apollo/client/link/error';
-import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, from, ApolloLink } from '@apollo/client';
+
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      'apollo-require-preflight': true
+    }
+  }));
+
+  return forward(operation);
+})
+
+const url = process.env.GRAPHQL_API
+console.log(url)
 
 export const client = new ApolloClient({
   // https://www.apollographql.com/docs/react/api/link/apollo-link-error/#gatsby-focus-wrapper
   /// separates graphql errors from network errors and creates readable error messages
-  link: ApolloLink.from([
+  link: from([
+    authMiddleware,
     onError(({ graphQLErrors, networkError }) => {
       if (graphQLErrors)
         graphQLErrors.forEach(({ message, locations, path }) =>
@@ -13,8 +30,8 @@ export const client = new ApolloClient({
       if (networkError) console.log(`[Network error]: ${networkError}`);
     }),
     new HttpLink({
-      uri: process.env.GRAPHQL_API
-    }),
+      uri: url
+    })
   ]),
   // https://www.apollographql.com/docs/react/caching
   /// allows for caching of fetched data to avoid unnecessary network requests
@@ -23,5 +40,5 @@ export const client = new ApolloClient({
   }),
   // https://www.apollographql.com/docs/react/data/suspense/#rendering-partial-data
   /// renders cached data right away, then re-renders with full data when the query completes
-  returnPartialData: true,
+  returnPartialData: true
 });
