6df8d614c8b0949e6a2644eee4212fab4cf91a94
escobard <escobard90@gmail.com>
2023-12-05 17:00:09 -0700

    feat: add semantics and roboto font stylesheets

--- client/ui/src/app/layout.js
+++ client/ui/src/app/layout.js
@@ -11,6 +11,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+      <link
+        rel="stylesheet"
+        href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css"
+      />
+      <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+    </head>
       <body className={inter.className}>
         <NotesProvider>
           {children}
