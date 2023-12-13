19269587d179df47b0c2889bb5236ae978c7fde3
escobard <escobard90@gmail.com>
2023-12-13 16:26:17 -0700

    fix: typos in getNoteByID with cache

--- server/graphql/resolvers/helpers/getNoteByID.js
+++ server/graphql/resolvers/helpers/getNoteByID.js
@@ -18,24 +18,21 @@ const getNoteByID = async (parent, noteId) => {
             // sets unix timestamp dates from postgres to js readable dates
             databaseNote[0].createdAt = new Date(databaseNote[0].createdAt).toLocaleString("en-US")
             databaseNote[0].updatedAt = new Date(databaseNote[0].updatedAt).toLocaleString("en-US")
-            // commenting out until there is logic to remove notes
-            // noteToCamelcase.removedAt = new Date(noteToCamelcase.removedAt).toLocaleString("en-US")
-            console.log('DATABASE NOTE', databaseNote[0])
+
             // populate cache with new note
             /// if cache does not have keys, assume cache is empty and add all existing notes + new notes
             if (cache.keys().length === 0){
                 let response = await knex('notes.notes').select('*');
+
                 // populate cache with DB data after initial fetch
                 response.map(n => {
                     // changes keys to camelCase for to process as JS
                     let noteToCamelcase = _.mapKeys(n, (v, k) => _.camelCase(k))
 
-                    // sets unix timestamp dates from postgres to js readable dates
                     noteToCamelcase.createdAt = new Date(noteToCamelcase.createdAt).toLocaleString("en-US")
                     noteToCamelcase.updatedAt = new Date(noteToCamelcase.updatedAt).toLocaleString("en-US")
-                    // commenting out until there is logic to remove notes
-                    // noteToCamelcase.removedAt = new Date(noteToCamelcase.removedAt).toLocaleString("en-US")
-                    cache.set(n.id, n)
+
+                    cache.set(noteToCamelcase.id, noteToCamelcase)
                 })
                 cache.set(databaseNote[0].id, databaseNote[0])
             }
