f002581d336f28bfb58ed30e11792d6271aac87c
escobard <escobard90@gmail.com>
2023-12-13 16:27:03 -0700

    feat: update createNote mutation with camelCase and unix timestamp to js date

--- server/graphql/resolvers/helpers/createNote.js
+++ server/graphql/resolvers/helpers/createNote.js
@@ -1,13 +1,23 @@
-import { knexConnector } from "../../utils/knexConnector.js";
+import _ from "lodash";
 
+import { knexConnector } from "../../utils/knexConnector.js";
 import cache from "../../cache.js"
+
 const createNote = async (parent, newNote) => {
     try{
 
         const note = { subject: newNote.subject, note: newNote.note }
         const knex = knexConnector();
         // ask knex to return all column results from insert, so data can be used to create a note in cache
-        const createdNote = await knex('notes.notes').insert(note).returning('*');
+        let createdNote = await knex('notes.notes').insert(note).returning('*');
+
+        // TODO - big gotcha - if code preceding transaction to insert fails, a note is created in the database anyway even if server crashes!
+        // /
+        createdNote[0] = _.mapKeys(createdNote[0], (v, k) => _.camelCase(k))
+
+        createdNote[0].createdAt = new Date(createdNote[0].createdAt).toLocaleString("en-US")
+        createdNote[0].updatedAt = new Date(createdNote[0].updatedAt).toLocaleString("en-US")
+
         // creates a note in cache after inserting data to db
         cache.set(createdNote[0].id, createdNote[0]);
         return createdNote[0];
