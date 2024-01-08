315fafef0f68246b878435c8910f1df2a9b2c212
escobard <escobard90@gmail.com>
2024-01-08 06:15:57 -0700

    chore: fix findDOMNode warning in dev

--- client/ui/next.config.js
+++ client/ui/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // disabling react strict mode to avoid findDOMNode warning in dev - this warning will be fixed with typescript version
+  reactStrictMode: false,
+}
 
 module.exports = nextConfig
