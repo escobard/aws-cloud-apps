c33bcad3ea5410f5817f57e833310c7e59e8f2ed
escobard <escobard90@gmail.com>
2024-01-12 20:17:42 -0700

    test: fix useAlert import

--- client/ui/src/hooks/useAlert/useAlert.js
+++ client/ui/src/hooks/useAlert/useAlert.js
@@ -1,9 +1,7 @@
 import { useState } from "react";
 
-const useAlert = () => {
+export const useAlert = () => {
   const [alert, setAlert] = useState({});
 
   return { alert, setAlert };
 };
-
-export default useAlert;
