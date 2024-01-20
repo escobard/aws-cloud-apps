29a9c94c48a44e2ec2e410ac1e233993acb372e3
escobard <escobard90@gmail.com>
2024-01-20 13:31:01 -0700

    refact: make query more readable in getNotes

--- server/tests/tests/getNotes.test.js
+++ server/tests/tests/getNotes.test.js
@@ -1,10 +1,14 @@
+
+// Warning - deep assertions on this test will fail unless addNotes.test.js is run first
 export default describe("> getNotes query", () => {
-  it(">> smoke test, api is alive and healthy", async () => {
-    let {status, body} = await request(server).post('/graphql').send({
-      "query": "query GetNotes {getNotes {note subject createdAt id removedAt updatedAt }}",
-      "variables": {},
-      "operationName": "GetNotes"
-    });
+  it(">> fetches notes from api and confirms expected types & values", async () => {
+
+    let query = {
+      query: "query GetNotes {getNotes {note subject createdAt id removedAt updatedAt }}",
+      variables: {},
+      operationName: "GetNotes"
+    }
+    let {status, body} = await request(server).post('/graphql').send(query);
     expect(status).toEqual(200)
 
     // validate that the response data has the expected properties & types
