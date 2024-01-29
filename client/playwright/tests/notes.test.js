1e3b128d21f5d5f63a89db927635e40c31589b5b
escobard <escobard90@gmail.com>
2024-01-29 12:28:41 -0700

    test: add test to validate Note web app title and add note button

---
+++ client/playwright/tests/notes.test.js
@@ -0,0 +1,20 @@
+// @ts-check
+const { describe, test, expect } = require('@playwright/test');
+
+describe('Notes landing page smoke tests', () => {
+  test('has title - basic HTMl has loaded', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Notes/);
+  });
+
+  test('has add notes button - API & Database have loaded', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+
+    // Expect a title "to contain" a substring.
+    await expect(page.getByRole('button', { name: 'Add note' })).toBeVisible();
+  });
+
+
+});
