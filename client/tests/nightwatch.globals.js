57a9d761979425ab0245125684056ea97913638e
escobard <escobard90@gmail.com>
2024-02-07 11:18:33 -0700

    feat: delete nightwatch tests directory

--- client/tests/nightwatch.globals.js
+++
@@ -1,8 +0,0 @@
-import { client } from "nightwatch-api";
-const { getQueriesFrom } = require("@testing-library/nightwatch");
-
-global.client = client;
-global.getQueriesFrom = getQueriesFrom;
-
-// screens
-global.landingPage = client.page.Home();
--- client/tests/tests/Home/Home.happy.test.js
+++
@@ -1,9 +0,0 @@
-describe("Home happy path", () => {
-  it(">> creates a note", async () => {
-    const subject = "Test subject";
-    const note = "Test note paragraph to pass validation";
-    await landingPage.navigate().createNote(client, { subject, note });
-    await landingPage.navigate().assertNote(client, subject);
-    client.end();
-  });
-});
